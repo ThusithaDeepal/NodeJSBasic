eaa4d192a02762a35b8dbe54af469eed1d0e18af
thusitha d <thusithadwini@gmail.com>
2020-11-10 12:27:55 +0530

    read write mkdir rmdir unlink done

--- readwrite.js
+++ readwrite.js
@@ -7,10 +7,43 @@ var fs = require('fs');
 
 
 
-fs.readFile('readfile.txt', 'utf8', function (err, data) {
+// fs.readFile('readfile.txt', 'utf8', function (err, data) {
 
-  fs.writeFile('writeFile.txt', data, function () {
+//   fs.writeFile('writeFile.txt', data, function () {
+
+//   })
+
+// })
+
+
+
+
+// fs.unlinkSync('writeFile.txt')
+
+// fs.unlink('writeFile.txt', function () {
+//   console.log("file deleted");
+// })
+
+fs.mkdir('newdir', function () {
+
+  fs.readFile('readFile.txt', 'utf8', function (err, data) {
+    console.log("file readed");
+
+    fs.writeFile('newdir/writeFile.txt', data, function () {
+      console.log("write file done")
+
+      //now delete dir so we need to remove file insilde dir
+
+      fs.unlink('./newdir/writeFile.txt', function () {
+        console.log("file inside directory deleted")
+
+        fs.rmdir('./newdir', function () {
+          console.log("created directory deleted");
+        })
+      })
+
+    })
 
   })
-
 })
+
