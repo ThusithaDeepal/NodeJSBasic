64110697853aac9d2a37bffb6562a67a6c53cb25
thusitha d <thusithadwini@gmail.com>
2020-11-11 12:59:57 +0530

    nodejs read write stream

--- stream.js
+++ stream.js
@@ -1,8 +1,11 @@
 var fs = require('fs');
 
 var myreadStream = fs.ReadStream(__dirname + '/readStream.txt', 'utf8');
+var mywriteStream = fs.WriteStream(__dirname + '/writeStream.txt');
 
 myreadStream.on('data', function (chunk) {
   console.log("stream recived")
   console.log(chunk);
+
+  mywriteStream.write(chunk);
 })
