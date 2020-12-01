d632fd1200831bdcf4d5feaf082cd3aebebe1c01
thusitha d <thusithadwini@gmail.com>
2020-12-01 13:55:29 +0530

    pipes

---
+++ pipes.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+
+
+// var myReadStream = fs.createReadStream(__dirname + '/readStream.txt', 'utf8');
+// var mywriteStream = fs.WriteStream(__dirname + '/writeStream.txt');
+
+// myReadStream.pipe(mywriteStream);
+
+
+http.createServer(function (req, res) {
+
+  var myReadStream = fs.createReadStream(__dirname + '/readStream.txt', 'utf8');
+  var mywriteStream = fs.WriteStream(__dirname + '/writeStream.txt');
+
+
+
+  console.log(req.url);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  myReadStream.pipe(res);
+
+
+}).listen(8080, '127.0.0.1');
+
+console.log("listen to port 8080");
