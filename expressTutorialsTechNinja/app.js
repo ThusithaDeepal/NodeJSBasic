9b19d1313a81fd78a0350482854b86432e6e3ad8
thusitha d <thusithadwini@gmail.com>
2020-12-07 12:45:15 +0530

    express route params

--- expressTutorialsTechNinja/app.js
+++ expressTutorialsTechNinja/app.js
@@ -10,4 +10,9 @@ app.get("/", function (req, res) {
 app.get('/Contact-Us', function (req, res) {
   res.send('contactus page');
 })
+
+app.get('/profile/:id', function (req, res) {
+  res.send('you have requested profile with id' + req.params.id
+  );
+})
 app.listen(8080);
