acf4e6cde9117ea320bd7e52c56174781ba773ac
thusitha d <thusithadwini@gmail.com>
2020-12-09 10:58:48 +0530

    templates

--- expressTutorialsTechNinja/app.js
+++ expressTutorialsTechNinja/app.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs'); //need to define template enjine it automatically access view folder
 
 
 
 app.get("/", function (req, res) {
-  res.send("default page");
+  // res.send("default page");
+  res.sendFile(__dirname + "/home.html");
 })
 
 app.get('/Contact-Us', function (req, res) {
-  res.send('contactus page');
+  // res.send('contactus page');
+  res.sendFile(__dirname + "/contactus.html");
 })
 
-app.get('/profile/:id', function (req, res) {
-  res.send('you have requested profile with id' + req.params.id
-  );
+app.get('/profile/:name', function (req, res) {
+  // res.send('you have requested profile with id' + req.params.id
+  // );
+  var person = { age: 20, sex: 'M', hobbies: ['reading', 'writing', 'singing'] };
+  res.render('profile.ejs', { name: req.params.name, data: person });
 })
 app.listen(8080);
