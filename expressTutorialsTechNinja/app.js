684739f56dc8abcbb8af2db00f6a4df5d2081519
thusitha d <thusithadwini@gmail.com>
2021-02-12 12:19:05 +0530

    contact form post

--- expressTutorialsTechNinja/app.js
+++ expressTutorialsTechNinja/app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-app.set('view engine', 'ejs'); //need to define template enjine it automatically access view folder
 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs'); //need to define template enjine it automatically access view folder
 app.use('/assets', express.static('assets'));
 
 app.get("/", function (req, res) {
@@ -13,7 +17,13 @@ app.get("/", function (req, res) {
 app.get('/Contact-Us', function (req, res) {
   // res.send('contactus page');
   // res.sendFile(__dirname + "/contactus.html");
-  res.render('contactus.ejs');
+  console.log(req.query['name'])
+  res.render('contactus.ejs', { qs: req.query });
+})
+
+app.post('/Contact-Us', urlencodedParser, function (req, res) {
+  console.log(req.body)
+  res.render('contactus_success', { qs: req.body });
 })
 
 app.get('/profile/:name', function (req, res) {
